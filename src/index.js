92dd3b268b0424bc104b79a0d94a1c0731748351
Martina Ciccanti <ciccanti88@gmail.com>
2023-11-15 14:05:16 +0100

    Added more weather data using API

--- src/index.js
+++ src/index.js
@@ -2,13 +2,32 @@ function sayWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let windSpeed = response.data.wind.speed;
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML= Math.round(temperature);
-
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity}%`
+    windSpeedElement.innerHTML = `Wind: ${Math.round(windSpeed)}km/h`;
 }
 
+function formatDate(date) {
+   let minutes = date.getMinutes();
+   let hours = date.getHours();
+   let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+   let day = days[date.getDay()];
 
+   if (minutes < 10) {
+    minutes = `0${minutes}`;
+   }
+    return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
     let apiKey = "1cb5e663abf44cab523b022o3700073t";
@@ -19,10 +38,8 @@ function searchCity(city) {
 function operateSearchInput(event){
     event.preventDefault();
     let searchFormInputElement = document.querySelector("#search-form-input");
-    
     searchCity(searchFormInputElement.value);
 }
-
 
 let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", operateSearchInput);
